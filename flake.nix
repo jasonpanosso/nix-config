5ca25ce60e968169da0c2d7e714927d094b09193
jasonpanosso <jasonpanosso@protonmail.com>
2024-03-03 13:49:35 -0500

    simplify config, add shell.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-  description = "Jason Panosso's system configurations";
+  description = "My NixOS configuration";
 
   nixConfig = {
-    substituters = [
+    extra-substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
@@ -19,6 +19,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     nixvim = {
       url = "github:nix-community/nixvim";
@@ -26,24 +31,34 @@
     };
   };
 
-  outputs = inputs @ { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
     let
-      system = "x86_64-linux";
-      overlays = [
-        (final: prev: {
-          lib = prev.lib // { my = import ./lib { inherit (final) lib; }; };
-        })
-        inputs.neovim-nightly-overlay.overlay
+      inherit (self) outputs;
+      lib = nixpkgs.lib // home-manager.lib;
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
       ];
 
-      pkgs = import nixpkgs {
-        inherit overlays system;
+      pkgsFor = lib.genAttrs systems (system: import nixpkgs {
+        inherit system;
         config.allowUnfree = true;
-      };
-
-      inherit (pkgs) lib;
+      });
+      forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
     in
     {
-      nixosConfigurations = import ./hosts { inherit system pkgs lib inputs; };
+      overlays = import ./overlays { inherit inputs outputs; };
+
+      packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
+      devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
+      formatter = forEachSystem (pkgs: pkgs.nixpkgs-fmt);
+
+      nixosConfigurations = {
+        # Laptop - XPS 13
+        pathfinder = lib.nixosSystem {
+          modules = [ ./hosts/pathfinder ];
+          specialArgs = { inherit inputs outputs; };
+        };
+      };
     };
 }
