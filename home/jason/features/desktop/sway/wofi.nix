36a27a56f019e064cdab1eebf50b4731fd54365c
jasonpanosso <jasonpanosso@protonmail.com>
2024-03-05 12:49:19 -0500

    add css styles for wofi

--- home/jason/features/desktop/sway/wofi.nix
+++ home/jason/features/desktop/sway/wofi.nix
@@ -10,12 +10,51 @@
     });
     settings = {
       image_size = 48;
-      columns = 3;
+      columns = 1;
       allow_images = true;
       insensitive = true;
+      key_up = "Ctrl-p";
+      key_down = "Ctrl-n";
       run-always_parse_args = true;
       run-cache_file = "/dev/null";
       run-exec_search = true;
+      show = "drun";
+      prompt = "";
     };
+
+    style = /* css */ ''
+      * {
+        font-family: monospace;
+      }
+
+      window {
+        background-color: #323232;
+      }
+
+      #input {
+        margin: 5px;
+        border-radius: 0px;
+        border: none;
+        background-color: #323232;
+        color: white;
+      }
+
+      #scroll {}
+
+      #text {
+        padding: 5px;
+        color: white;
+      }
+
+      #entry {
+        background-color: #323232;
+      }
+
+      #entry:selected {
+        background-color: #4c7899;
+      }
+
+      #text:selected {}
+    '';
   };
 }
