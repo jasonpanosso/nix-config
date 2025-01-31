20fb8d4b4814191358ec1900d13ac85d058b7bcd
jasonpanosso <jasonpanosso@protonmail.com>
2025-01-31 11:23:06 -0500

    feat: cleanup clock

--- home/jason/features/desktop/hyprland/waybar.nix
+++ home/jason/features/desktop/hyprland/waybar.nix
@@ -42,13 +42,28 @@ in
             "" # Icon: battery-three-quarters
             "" # Icon: battery-full
           ];
-          "tooltip" = true;
+          tooltip = true;
         };
 
         clock = {
           interval = 1;
           format = "󰃰   {:%b %d %H:%M}"; # Icon: calendar/clock
-          tooltip-format = "{:%B %e %Y}";
+          timezone = "America/Detroit";
+          tooltip = true;
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "year";
+            mode-mon-col = 3;
+            weeks-pos = "right";
+            on-scroll = 1;
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
         };
 
         cpu = {
